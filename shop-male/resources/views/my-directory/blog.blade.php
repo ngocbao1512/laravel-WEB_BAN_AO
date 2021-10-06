4613bf5ef7896e6be7b6e1d02214d2d3b40f1e1a
nguyenthuphuong <Phuongnyo98@gmail.com>
2021-10-06 14:15:26 +0700

    add theme

---
+++ shop-male/resources/views/my-directory/blog.blade.php
@@ -0,0 +1,192 @@
+<x-male-shop>
+
+<!-- Breadcrumb Section Begin -->
+<section class="breadcrumb-blog set-bg" data-setbg="/male-shop/img/breadcrumb-bg.jpg">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h2>Our Blog</h2>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Breadcrumb Section End -->
+
+    <!-- Blog Section Begin -->
+    <section class="blog spad">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 col-md-6 col-sm-6">
+                    <div class="blog__item">
+                        <div class="blog__item__pic set-bg" data-setbg="/male-shop/img/blog/blog-1.jpg"></div>
+                        <div class="blog__item__text">
+                            <span><img src="/male-shop/img/icon/calendar.png" alt=""> 16 February 2020</span>
+                            <h5>What Curling Irons Are The Best Ones</h5>
+                            <a href="#">Read More</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-6">
+                    <div class="blog__item">
+                        <div class="blog__item__pic set-bg" data-setbg="/male-shop/img/blog/blog-2.jpg"></div>
+                        <div class="blog__item__text">
+                            <span><img src="/male-shop/img/icon/calendar.png" alt=""> 21 February 2020</span>
+                            <h5>Eternity Bands Do Last Forever</h5>
+                            <a href="#">Read More</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-6">
+                    <div class="blog__item">
+                        <div class="blog__item__pic set-bg" data-setbg="/male-shop/img/blog/blog-3.jpg"></div>
+                        <div class="blog__item__text">
+                            <span><img src="/male-shop/img/icon/calendar.png" alt=""> 28 February 2020</span>
+                            <h5>The Health Benefits Of Sunglasses</h5>
+                            <a href="#">Read More</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-6">
+                    <div class="blog__item">
+                        <div class="blog__item__pic set-bg" data-setbg="/male-shop/img/blog/blog-4.jpg"></div>
+                        <div class="blog__item__text">
+                            <span><img src="/male-shop/img/icon/calendar.png" alt=""> 16 February 2020</span>
+                            <h5>Aiming For Higher The Mastopexy</h5>
+                            <a href="#">Read More</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-6">
+                    <div class="blog__item">
+                        <div class="blog__item__pic set-bg" data-setbg="/male-shop/img/blog/blog-5.jpg"></div>
+                        <div class="blog__item__text">
+                            <span><img src="/male-shop/img/icon/calendar.png" alt=""> 21 February 2020</span>
+                            <h5>Wedding Rings A Gift For A Lifetime</h5>
+                            <a href="#">Read More</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-6">
+                    <div class="blog__item">
+                        <div class="blog__item__pic set-bg" data-setbg="/male-shop/img/blog/blog-6.jpg"></div>
+                        <div class="blog__item__text">
+                            <span><img src="/male-shop/img/icon/calendar.png" alt=""> 28 February 2020</span>
+                            <h5>The Different Methods Of Hair Removal</h5>
+                            <a href="#">Read More</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-6">
+                    <div class="blog__item">
+                        <div class="blog__item__pic set-bg" data-setbg="/male-shop/img/blog/blog-7.jpg"></div>
+                        <div class="blog__item__text">
+                            <span><img src="/male-shop/img/icon/calendar.png" alt=""> 16 February 2020</span>
+                            <h5>Hoop Earrings A Style From History</h5>
+                            <a href="#">Read More</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-6">
+                    <div class="blog__item">
+                        <div class="blog__item__pic set-bg" data-setbg="/male-shop/img/blog/blog-8.jpg"></div>
+                        <div class="blog__item__text">
+                            <span><img src="/male-shop/img/icon/calendar.png" alt=""> 21 February 2020</span>
+                            <h5>Lasik Eye Surgery Are You Ready</h5>
+                            <a href="#">Read More</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-6">
+                    <div class="blog__item">
+                        <div class="blog__item__pic set-bg" data-setbg="/male-shop/img/blog/blog-9.jpg"></div>
+                        <div class="blog__item__text">
+                            <span><img src="/male-shop/img/icon/calendar.png" alt=""> 28 February 2020</span>
+                            <h5>Lasik Eye Surgery Are You Ready</h5>
+                            <a href="#">Read More</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Blog Section End -->
+
+    <!-- Footer Section Begin -->
+    <footer class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-6 col-sm-6">
+                    <div class="footer__about">
+                        <div class="footer__logo">
+                            <a href="#"><img src="/male-shop/img/footer-logo.png" alt=""></a>
+                        </div>
+                        <p>The customer is at the heart of our unique business model, which includes design.</p>
+                        <a href="#"><img src="/male-shop/img/payment.png" alt=""></a>
+                    </div>
+                </div>
+                <div class="col-lg-2 offset-lg-1 col-md-3 col-sm-6">
+                    <div class="footer__widget">
+                        <h6>Shopping</h6>
+                        <ul>
+                            <li><a href="#">Clothing Store</a></li>
+                            <li><a href="#">Trending Shoes</a></li>
+                            <li><a href="#">Accessories</a></li>
+                            <li><a href="#">Sale</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-2 col-md-3 col-sm-6">
+                    <div class="footer__widget">
+                        <h6>Shopping</h6>
+                        <ul>
+                            <li><a href="#">Contact Us</a></li>
+                            <li><a href="#">Payment Methods</a></li>
+                            <li><a href="#">Delivary</a></li>
+                            <li><a href="#">Return & Exchanges</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-3 offset-lg-1 col-md-6 col-sm-6">
+                    <div class="footer__widget">
+                        <h6>NewLetter</h6>
+                        <div class="footer__newslatter">
+                            <p>Be the first to know about new arrivals, look books, sales & promos!</p>
+                            <form action="#">
+                                <input type="text" placeholder="Your email">
+                                <button type="submit"><span class="icon_mail_alt"></span></button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <div class="footer__copyright__text">
+                        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                        <p>Copyright ©
+                            <script>
+                                document.write(new Date().getFullYear());
+                            </script>2020
+                            All rights reserved | This template is made with <i class="fa fa-heart-o"
+                                aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+                        </p>
+                        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- Footer Section End -->
+
+    <!-- Search Begin -->
+    <div class="search-model">
+        <div class="h-100 d-flex align-items-center justify-content-center">
+            <div class="search-close-switch">+</div>
+            <form class="search-model-form">
+                <input type="text" id="search-input" placeholder="Search here.....">
+            </form>
+        </div>
+    </div>
+    <!-- Search End -->
+
+</x-male-shop>
--- shop-male/routes/web.php
+++ shop-male/routes/web.php
@@ -16,3 +16,31 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home-page', function () {
+    return view('home-page');
+});
+Route::get('/shop', function () {
+    return view('my-directory.shop');
+});
+Route::get('/about', function () {
+    return view('my-directory.about');
+});
+Route::get('/shop-details', function () {
+    return view('my-directory.shop-details');
+});
+Route::get('/shop-cart', function () {
+    return view('my-directory.shop-cart');
+});
+Route::get('/checkout', function () {
+    return view('my-directory.checkout');
+});
+Route::get('/blog-details', function () {
+    return view('my-directory.blog-details');
+});
+Route::get('/blog', function () {
+    return view('my-directory.blog');
+});
+Route::get('/contact', function () {
+    return view('my-directory.contact');
+});
