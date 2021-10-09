d9b835420e0b85d3ef2aca3048dc4f2c4ad7a06f
Nguyễn Thu Phương <77055859+nguyenthuphuon@users.noreply.github.com>
2021-10-09 15:16:39 +0700

    Change theme admin (#13)
    * Add theme admin
    
    * edit theme admin

---
+++ shop-male/resources/views/my-admin/categories/index.blade.php
@@ -0,0 +1,208 @@
+<x-admin-lte>
+
+<div class="container mt-5">
+      <div class="row tm-content-row">
+        <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12 tm-block-col">
+          <div class="tm-bg-primary-dark tm-block tm-block-categorys">
+            <div class="tm-category-table-container">
+              <table class="table table-hover tm-table-small tm-category-table">
+                <thead>
+                  <tr>
+                    <th scope="col">&nbsp;</th>
+                    <th scope="col">Category NAME</th>
+                    <th scope="col">UNIT SOLD</th>
+                    <th scope="col">IN STOCK</th>
+                    <th scope="col">EXPIRE DATE</th>
+                    <th scope="col">&nbsp;</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td class="tm-category-name">Lorem Ipsum Category 1</td>
+                    <td>1,450</td>
+                    <td>550</td>
+                    <td>28 March 2019</td>
+                    <td>
+                      <a href="#" class="tm-category-edit-link">
+                        <i class="far fa-trash-alt tm-category-edit-icon"></i>
+                      </a>
+                    </td>
+                    <td>
+                      <a href="#" class="tm-category-delete-link">
+                        <i class="far fa-trash-alt tm-category-delete-icon"></i>
+                      </a>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td class="tm-category-name">Lorem Ipsum Category 2</td>
+                    <td>1,250</td>
+                    <td>750</td>
+                    <td>21 March 2019</td>
+                    <td>
+                      <a href="#" class="tm-category-edit-link">
+                        <i class="far fa-trash-alt tm-category-edit-icon"></i>
+                      </a>
+                    </td>
+                    <td>
+                      <a href="#" class="tm-category-delete-link">
+                        <i class="far fa-trash-alt tm-category-delete-icon"></i>
+                      </a>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td class="tm-category-name">Lorem Ipsum Category 3</td>
+                    <td>1,100</td>
+                    <td>900</td>
+                    <td>18 Feb 2019</td>
+                    <td>
+                      <a href="#" class="tm-category-edit-link">
+                        <i class="far fa-trash-alt tm-category-edit-icon"></i>
+                      </a>
+                    </td>
+                    <td>
+                      <a href="#" class="tm-category-delete-link">
+                        <i class="far fa-trash-alt tm-category-delete-icon"></i>
+                      </a>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td class="tm-category-name">Lorem Ipsum Category 4</td>
+                    <td>1,400</td>
+                    <td>600</td>
+                    <td>24 Feb 2019</td>
+                    <td>
+                      <a href="#" class="tm-category-edit-link">
+                        <i class="far fa-trash-alt tm-category-edit-icon"></i>
+                      </a>
+                    </td>
+                    <td>
+                      <a href="#" class="tm-category-delete-link">
+                        <i class="far fa-trash-alt tm-category-delete-icon"></i>
+                      </a>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td class="tm-category-name">Lorem Ipsum Category 5</td>
+                    <td>1,800</td>
+                    <td>200</td>
+                    <td>22 Feb 2019</td>
+                    <td>
+                      <a href="#" class="tm-category-edit-link">
+                        <i class="far fa-trash-alt tm-category-edit-icon"></i>
+                      </a>
+                    </td>
+                    <td>
+                      <a href="#" class="tm-category-delete-link">
+                        <i class="far fa-trash-alt tm-category-delete-icon"></i>
+                      </a>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td class="tm-category-name">Lorem Ipsum Category 6</td>
+                    <td>1,000</td>
+                    <td>1,000</td>
+                    <td>20 Feb 2019</td>
+                    <td>
+                      <a href="#" class="tm-category-edit-link">
+                        <i class="far fa-trash-alt tm-category-edit-icon"></i>
+                      </a>
+                    </td>
+                    <td>
+                      <a href="#" class="tm-category-delete-link">
+                        <i class="far fa-trash-alt tm-category-delete-icon"></i>
+                      </a>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td class="tm-category-name">Lorem Ipsum Category 7</td>
+                    <td>500</td>
+                    <td>100</td>
+                    <td>10 Feb 2019</td>
+                    <td>
+                      <a href="#" class="tm-category-edit-link">
+                        <i class="far fa-trash-alt tm-category-edit-icon"></i>
+                      </a>
+                    </td>
+                    <td>
+                      <a href="#" class="tm-category-delete-link">
+                        <i class="far fa-trash-alt tm-category-delete-icon"></i>
+                      </a>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td class="tm-category-name">Lorem Ipsum Category 8</td>
+                    <td>1,000</td>
+                    <td>600</td>
+                    <td>08 Feb 2019</td>
+                    <td>
+                      <a href="#" class="tm-category-edit-link">
+                        <i class="far fa-trash-alt tm-category-edit-icon"></i>
+                      </a>
+                    </td>
+                    <td>
+                      <a href="#" class="tm-category-delete-link">
+                        <i class="far fa-trash-alt tm-category-delete-icon"></i>
+                      </a>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td class="tm-category-name">Lorem Ipsum Category 9</td>
+                    <td>1,200</td>
+                    <td>800</td>
+                    <td>24 Jan 2019</td>
+                    <td>
+                      <a href="#" class="tm-category-edit-link">
+                        <i class="far fa-trash-alt tm-category-edit-icon"></i>
+                      </a>
+                    </td>
+                    <td>
+                      <a href="#" class="tm-category-delete-link">
+                        <i class="far fa-trash-alt tm-category-delete-icon"></i>
+                      </a>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td class="tm-category-name">Lorem Ipsum Category 10</td>
+                    <td>1,600</td>
+                    <td>400</td>
+                    <td>22 Jan 2019</td>
+                    <td>
+                      <a href="#" class="tm-category-edit-link">
+                        <i class="far fa-trash-alt tm-category-edit-icon"></i>
+                      </a>
+                    </td>
+                    <td>
+                      <a href="#" class="tm-category-delete-link">
+                        <i class="far fa-trash-alt tm-category-delete-icon"></i>
+                      </a>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td class="tm-category-name">Lorem Ipsum Category 11</td>
+                    <td>2,000</td>
+                    <td>400</td>
+                    <td>21 Jan 2019</td>
+                    <td>
+                      <a href="#" class="tm-category-edit-link">
+                        <i class="far fa-trash-alt tm-category-edit-icon"></i>
+                      </a>
+                    </td>
+                    <td>
+                      <a href="#" class="tm-category-delete-link">
+                        <i class="far fa-trash-alt tm-category-delete-icon"></i>
+                      </a>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <!-- table container -->
+            <a
+              href="add-category.html"
+              class="btn btn-primary btn-block text-uppercase mb-3">Add new category</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+</x-admin-lte>
--- shop-male/resources/views/my-admin/products/edit.blade.php
+++ shop-male/resources/views/my-admin/products/edit.blade.php
@@ -3,7 +3,7 @@
     <div class="row">
       <div class="col-12 mx-auto">
       <div class="float-center">
-              <p class="tm-block-title d-inline-block float-center" style="font-size: 30px">Add Product</p>
+              <p class="tm-block-title d-inline-block float-center" style="font-size: 30px">Edit Product</p>
             </div>
         <div class="tm-bg-primary-dark tm-block tm-block-h-auto">
           <div class="row tm-edit-product-row">
@@ -13,8 +13,12 @@
                 <div class="row">
                    <div class=" col-xs-12 col-sm-6">
                    <label for="name"><b>Product Name</b></label>
-                  <input id="name" name="name" type="text"  placeholder="Enter product name" class="form-control validate" />
+                  <input id="name" name="name" type="text" value="Lorem Ipsum Product" class="form-control validate" />
                    </div>
+                   <!-- <div class=" col-xs-12 col-sm-6">
+                    <label for="brand"><b>Rate</b></label>&ensp;
+                   <span><i class="fas fa-star" style="color: white"></i></span>
+                   </div> -->
                  </div>
                 </div>
                 <hr>
@@ -42,22 +46,23 @@
                 <div class="row">
                   <div class="form-group  col-xs-12 col-sm-6">
                     <label for="price"><b>Price</b></label>
-                    <input id="price" name="price" type="text" placeholder="Enter price" class="form-control validate" />
+                    <input id="price" name="price" type="text" value="$100" class="form-control validate" />
                   </div>
                   <div class="form-group  col-xs-12 col-sm-6">
                     <label for="sale_off"><b>Sale Of</b></label>
-                    <input id="sale_off" name="sale_off" type="text" placeholder="Enter sale off" class="form-control validate" />
+                    <input id="sale_off" name="sale_off" type="text" value="10%" class="form-control validate" />
                   </div>
                 </div>
                 <hr>
                 <div class="form-group ">
-                  <label for="description">Description</label>
-                  <textarea class="form-control validate" rows="3" placeholder="Enter descreption"></textarea>
+                  <label for="description"><b>Description</b></label>
+                  <textarea class="form-control validate tm-small" rows="3" required>Red Shirt</textarea>
                 </div>
                 
             </div>
             <div class="col-xl-6 col-lg-6 col-md-12 mx-auto mb-4">
-                <div class="tm-product-img-dummy mx-auto">
+                <div class="tm-product-img-edit mx-auto">
+                  <img src="/admin-lte/img/product-image.jpg"  style="width: 100%; height: 200%" alt="Product image" class="img-fluid d-block mx-auto">
                   <i
                     class="fas fa-cloud-upload-alt tm-upload-icon"
                     onclick="document.getElementById('fileInput').click();"
@@ -68,12 +73,13 @@
                   <input
                     type="button"
                     class="btn btn-primary btn-block mx-auto"
-                    value="UPLOAD PRODUCT IMAGE"
+                    value="CHANGE IMAGE NOW"
                     onclick="document.getElementById('fileInput').click();"
                   />
                 </div>
               </div>
-              <a  herf="/products" type="button" class="btn btn-primary btn-block text-uppercase" style="color:white">Add Product</a>
+            <div class="col-12">
+              <a  herf="/products" type="button" class="btn btn-primary btn-block text-uppercase" style="color:white">Edit Product</a>
             </div>
             </form>
           </div>
