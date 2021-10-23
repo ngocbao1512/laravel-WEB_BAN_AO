d17b6f503b53ebd861ded660dad3ca7eacf9cb5b
Ngoc Bao <63847215+ngocbao1512@users.noreply.github.com>
2021-10-23 12:02:59 +0700

    add feature login register  (#25)
    * add image
    
    * crud blog crud tag
    
    * conflix
    
    * resolve cfx
    
    * add feature login, register

--- shop-male/resources/views/auth/login.blade.php
+++ shop-male/resources/views/auth/login.blade.php
@@ -1,56 +1,145 @@
 <x-guest-layout>
+    
     <x-auth-card>
         <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
+            
         </x-slot>
 
-        <!-- Session Status -->
-        <x-auth-session-status class="mb-4" :status="session('status')" />
+        {{-- theme --}}
 
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+        
+        <!-- htlm -->
 
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
+        
 
-            <!-- Email Address -->
-            <div>
-                <x-label for="email" :value="__('Email')" />
+        <section class="forms-section">
+            <h1 class="section-title">Animated Forms</h1>
+            <div class="forms">
+              <div class="form-wrapper is-active">
+                <button type="button" class="switcher switcher-login">
+                  Login
+                  <span class="underline"></span>
+                </button>
 
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            </div>
+                <x-auth-session-status class="mb-4" :status="session('status')" />
 
-            <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
+                <!-- Validation Errors -->
+                <x-auth-validation-errors class="mb-4" :errors="$errors" />
 
-                <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="current-password" />
-            </div>
+                
+                <form class="form form-login" method="POST" action="{{ route('login') }}">
+                    @csrf
+                  <fieldset>
+                    <legend>Please, enter your email and password for login.</legend>
+                    <div class="input-block">
+                        <x-label for="login-email" :value="__('Email')" />
+        
+                        <x-input id="login-email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+                    </div>
+        
+                    <!-- Password -->
+                    <div class="mt-4 input-block">
+                        <x-label for="login-password" :value="__('Password')" />
 
-            <!-- Remember Me -->
-            <div class="block mt-4">
-                <label for="remember_me" class="inline-flex items-center">
-                    <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
+                        <x-input id="login-password" class="block mt-1 w-full"
+                            type="password"
+                            name="password"
+                            required autocomplete="current-password" />
+                    </div>
 
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
+                    <!-- Remember Me -->
+                    <div class="block mt-4">
+                        <label for="remember_me" class="inline-flex items-center">
+                            <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
+                            <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                        </label>
+                    </div>
+
+                    <div class="flex items-center justify-end mt-4">
+                        @if (Route::has('password.request'))
+                            <a class=" text-sm text-blue-600 hover:text-blue-900" href="{{ route('password.request') }}" >
+                                {{ __('Forgot your password?') }}
+                            </a>
+                        @endif
+        
+                        <x-button class="ml-3 btn-login">
+                            {{ __('Log in') }}
+                        </x-button>
+                    </div>
+                  </fieldset>
+                </form>
+              </div>
+
+              <!--register-->
+              
+              <div class="form-wrapper">
+                <button type="button" class="switcher switcher-signup">
+                  Sign Up
+                  <span class="underline"></span>
+                </button>
+                <form method="POST" action="{{ route('register') }}" class="form form-signup">
+                    @csrf
+                  <fieldset>
+                    <legend>Please, enter your email, password and password confirmation for sign up.</legend>
+                    <!-- Name -->
+                    <div class="input-block">
+                        <x-label for="signup-name" :value="__('Name')" />
+
+                        <x-input id="signup-name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
+                    </div>
+                    <!-- Email Address -->
+                    <div class="mt-4 input-block">
+                        <x-label for="signup-email" :value="__('Email')" />
+
+                        <x-input id="signup-email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
+                    </div>
+
+
+                    <!-- Password -->
+                    <div class="mt-4 input-block">
+                        <x-label for="signup-password" :value="__('Password')" />
+
+                        <x-input id="signup-password" class="block mt-1 w-full"
+                            type="password"
+                            name="password"
+                            required autocomplete="new-password" />
+                    </div>
+
+                    <!-- Confirm Password -->
+                    <div class="mt-4 input-block">
+                        <x-label for="signup-password-confirm" :value="__('Confirm Password')" />
+
+                        <x-input id="signup-password-confirm" class="block mt-1 w-full"
+                            type="password"
+                            name="password_confirmation" required />
+                    </div>
+
+                  </fieldset>
+
+                  <div class="flex items-center justify-end mt-4">
+                    <a class="underline text-sm text-gray-600 hover:text-gray-900 " href="{{ route('login') }}">
+                        {{ __('Already registered?') }}
                     </a>
-                @endif
-
-                <x-button class="ml-3">
-                    {{ __('Log in') }}
-                </x-button>
+    
+                    <x-button class="ml-4 btn-signup">
+                        {{ __('Register') }}
+                    </x-button>
+                </div>
+                </form>
+              </div>
             </div>
-        </form>
+          </section>
+          <script>
+            const switchers = [...document.querySelectorAll('.switcher')]
+
+            switchers.forEach(item => {
+                item.addEventListener('click', function() {
+                    switchers.forEach(item => item.parentElement.classList.remove('is-active'))
+                    this.parentElement.classList.add('is-active')
+                })
+            })
+          </script>
+
+        {{-- end theme --}}
     </x-auth-card>
 </x-guest-layout>
--- shop-male/resources/views/layouts/guest.blade.php
+++ shop-male/resources/views/layouts/guest.blade.php
@@ -15,9 +15,423 @@
 
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
+
+        <!-- Font awsome -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog==" crossorigin="anonymous" />
+  
+        <!-- css component about-web -->
+        <style>
+        @import url('https://fonts.googleapis.com/css?family=Muli&display=swap');
+        
+        .about-web {
+          grid-column: 1/2;
+          box-sizing: border-box;
+          font-family: 'Muli', sans-serif;
+          max-width: 60vw;
+          
+        }
+        
+        
+        
+        .faq-container {
+          max-width: 600px;
+          margin: 100px auto;
+          padding: 5px 25px;
+          border-radius: 35px;
+          background-color: #ffffff;
+
+        }
+
+        h1{
+          font-size: 30px;
+          font-weight: 900;
+          color: rgba(153, 153, 153, 0.856);
+          text-align: center;
+        }
+        
+        .faq {
+          background-color: transparent;
+          border: 1px solid #9fa4a8;
+          border-radius: 10px;
+          margin: 20px 0;
+          padding: 30px;
+          position: relative;
+          overflow: hidden;
+          transition: 0.3s ease;
+        }
+        
+        .faq.active {
+          background-color: #fff;
+          box-shadow: 0 3px 6px rgba(0, 0, 0, 0.1), 0 3px 6px rgba(0, 0, 0, 0.1);
+        }
+        
+        .faq.active::before,
+        .faq.active::after {
+          content: '\f075';
+          font-family: 'Font Awesome 5 Free';
+          color: #2ecc71;
+          font-size: 7rem;
+          position: absolute;
+          opacity: 0.2;
+          top: 20px;
+          left: 20px;
+          z-index: 0;
+        }
+        
+        .faq.active::before {
+          color: #3498db;
+          top: -10px;
+          left: -30px;
+          transform: rotateY(180deg);
+        }
+        
+        .faq-title {
+          margin: 0 35px 0 0;
+        }
+        
+        .faq-text {
+          display: none;
+          margin: 30px 0 0;
+        }
+        
+        .faq.active .faq-text {
+          display: block;
+        }
+        
+        .faq-toggle {
+          background-color: transparent;
+          border: 0;
+          border-radius: 50%;
+          cursor: pointer;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          font-size: 16px;
+          padding: 0;
+          position: absolute;
+          top: 30px;
+          right: 30px;
+          height: 30px;
+          width: 30px;
+        }
+        
+        .faq-toggle:focus {
+          outline: 0;
+        }
+        
+        .faq-toggle .fa-times {
+          display: none;
+        }
+        
+        .faq.active .faq-toggle .fa-times {
+          color: #fff;
+          display: block;
+        }
+        
+        .faq.active .faq-toggle .fa-chevron-down {
+          display: none;
+        }
+        
+        .faq.active .faq-toggle {
+          background-color: #9fa4a8;
+        }
+        
+        </style>
+        <!-- CSS OF LOGIN REGISTER -->
+        <style>
+       
+
+        body {
+            margin: 0;
+            font-family: Roboto, -apple-system, 'Helvetica Neue', 'Segoe UI', Arial, sans-serif;
+            background: #f3f4f6;
+        }
+
+        .forms-section {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+        }
+
+        .section-title {
+            font-size: 32px;
+            letter-spacing: 1px;
+            color: #fff;
+        }
+
+        .forms {
+            display: flex;
+            align-items: flex-start;
+            margin-top: 30px;
+        }
+
+        .form-wrapper {
+            animation: hideLayer .3s ease-out forwards;
+        }
+
+        .form-wrapper.is-active {
+            animation: showLayer .3s ease-in forwards;
+        }
+
+        @keyframes showLayer {
+            50% {
+                z-index: 1;
+            }
+            100% {
+                z-index: 1;
+            }
+        }
+
+        @keyframes hideLayer {
+            0% {
+                z-index: 1;
+            }
+            49.999% {
+                z-index: 1;
+            }
+        }
+
+        .switcher {
+            position: relative;
+            cursor: pointer;
+            display: block;
+            margin-right: auto;
+            margin-left: auto;
+            padding: 0;
+            text-transform: uppercase;
+            font-family: inherit;
+            font-size: 16px;
+            letter-spacing: .5px;
+            color: #999;
+            background-color: transparent;
+            border: none;
+            outline: none;
+            transform: translateX(0);
+            transition: all .3s ease-out;
+        }
+
+        .form-wrapper.is-active .switcher-login {
+            color: #fff;
+            transform: translateX(90px);
+        }
+
+        .form-wrapper.is-active .switcher-signup {
+            color: #fff;
+            transform: translateX(-90px);
+        }
+
+        .underline {
+            position: absolute;
+            bottom: -5px;
+            left: 0;
+            overflow: hidden;
+            pointer-events: none;
+            width: 100%;
+            height: 2px;
+        }
+
+        .underline::before {
+            content: '';
+            position: absolute;
+            top: 0;
+            left: inherit;
+            display: block;
+            width: inherit;
+            height: inherit;
+            background-color: currentColor;
+            transition: transform .2s ease-out;
+        }
+
+        .switcher-login .underline::before {
+            transform: translateX(101%);
+        }
+
+        .switcher-signup .underline::before {
+            transform: translateX(-101%);
+        }
+
+        .form-wrapper.is-active .underline::before {
+            transform: translateX(0);
+        }
+
+        .form {
+            overflow: hidden;
+            min-width: 260px;
+            margin-top: 50px;
+            padding: 30px 25px;
+        border-radius: 5px;
+            transform-origin: top;
+        }
+
+        .form-login {
+            animation: hideLogin .3s ease-out forwards;
+        }
+
+        .form-wrapper.is-active .form-login {
+            animation: showLogin .3s ease-in forwards;
+        }
+
+        @keyframes showLogin {
+            0% {
+                background: #d7e7f1;
+                transform: translate(40%, 10px);
+            }
+            50% {
+                transform: translate(0, 0);
+            }
+            100% {
+                background-color: #fff;
+                transform: translate(35%, -20px);
+            }
+        }
+
+        @keyframes hideLogin {
+            0% {
+                background-color: #fff;
+                transform: translate(35%, -20px);
+            }
+            50% {
+                transform: translate(0, 0);
+            }
+            100% {
+                background: #d7e7f1;
+                transform: translate(40%, 10px);
+            }
+        }
+
+        .form-signup {
+            animation: hideSignup .3s ease-out forwards;
+        }
+
+        .form-wrapper.is-active .form-signup {
+            animation: showSignup .3s ease-in forwards;
+        }
+
+        @keyframes showSignup {
+            0% {
+                background: #d7e7f1;
+                transform: translate(-40%, 10px) scaleY(.8);
+            }
+            50% {
+                transform: translate(0, 0) scaleY(.8);
+            }
+            100% {
+                background-color: #fff;
+                transform: translate(-35%, -20px) scaleY(1);
+            }
+        }
+
+        @keyframes hideSignup {
+            0% {
+                background-color: #fff;
+                transform: translate(-35%, -20px) scaleY(1);
+            }
+            50% {
+                transform: translate(0, 0) scaleY(.8);
+            }
+            100% {
+                background: #d7e7f1;
+                transform: translate(-40%, 10px) scaleY(.8);
+            }
+        }
+
+        .form fieldset {
+            position: relative;
+            opacity: 0;
+            margin: 0;
+            padding: 0;
+            border: 0;
+            transition: all .3s ease-out;
+            
+            
+        }
+
+        .form{
+          box-shadow: 2px -2px 8px #88888863;
+        }
+
+        .form-login fieldset {
+            transform: translateX(-50%);
+        }
+
+        .form-signup fieldset {
+            transform: translateX(50%);
+        }
+
+        .form-wrapper.is-active fieldset {
+            opacity: 1;
+            transform: translateX(0);
+            transition: opacity .4s ease-in, transform .35s ease-in;
+        }
+
+        .form legend {
+            position: absolute;
+            overflow: hidden;
+            width: 1px;
+            height: 1px;
+            clip: rect(0 0 0 0);
+        }
+
+        .input-block {
+            margin-bottom: 20px;
+        }
+
+        .input-block label {
+            font-size: 14px;
+        color: #a1b4b4;
+        }
+
+        .input-block input {
+            display: block;
+            width: 100%;
+            margin-top: 8px;
+            padding-right: 15px;
+            padding-left: 15px;
+            font-size: 16px;
+            line-height: 40px;
+            color: #3b4465;
+            background: #eef9fe;
+            border: 1px solid #cddbef;
+            border-radius: 2px;
+        }
+
+        .form [type='submit'] {
+            opacity: 0;
+            display: block;
+            min-width: 120px;
+            margin: 30px auto 10px;
+            font-size: 12px;
+            line-height: 20px;
+            border-radius: 25px;
+            border: none;
+            transition: all .3s ease-out;
+        }
+
+        .form-wrapper.is-active .form [type='submit'] {
+            opacity: 1;
+            transform: translateX(0);
+            transition: all .4s ease-in;
+        }
+
+        .btn-login {
+            color: #fbfdff;
+            background: #a7e245;
+            transform: translateX(-30%);
+        }
+
+        .btn-signup {
+            color: #a7e245;
+            background: #fbfdff;
+            box-shadow: inset 0 0 0 2px #a7e245;
+            transform: translateX(30%);
+        }
+
+        </style>
+
     </head>
     <body>
-        <div class="font-sans text-gray-900 antialiased">
+        <div class="font-sans text-gray-900 antialiased" style="display: grid; grid-template-columns: 2fr 1fr;">
             {{ $slot }}
         </div>
     </body>
